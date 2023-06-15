8257ca96f1b03dd0a6b94e8324221f067651716b
MrPakoras <hartanas8g@gmail.com>
2023-06-15 22:09:11 +0100

    Update CustomQuips.py

--- CustomQuips.py
+++ CustomQuips.py
@@ -7,7 +7,7 @@
 # Creates tts for questions (in .ogg format) - potentially implement FakeYou deepfake tts
 # Creates individual folders for each question with the audio file and data.jet
 
-# QuiplashQuestions.jet example - '{"content":[{"x":false,"id":35008,"prompt":"The gross thing nobody knows about the Easter Bunny"}],"episodeid":1251}'
+# QuiplashQuestion.jet example - '{"content":[{"x":false,"id":35008,"prompt":"The gross thing nobody knows about the Easter Bunny"}],"episodeid":1251}'
 # \u2019 = apostrophe, \u201c = opening speech mark, \u201d = closing speech mark
 
 import os, re
@@ -22,9 +22,9 @@ dirs = [item for item in os.listdir('./') if os.path.isdir(os.path.join('./', it
 cqldirs = [int(x.split('CQL ')[1]) for x in dirs if re.match(r'CQL .*',x)] # list of dirs starting with "CQL"
 cqldirs.append(0) # Adds 0, so list isnt empty
 newdir = 'CQL '+str(max(cqldirs)+1)
-os.mkdir(newdir)
+os.mkdir(newdir) # make newdir
 
-# ### Creating QuiplashQuestions.jet
+# ### Creating QuiplashQuestion.jet
 
 def createid(n): # Creates an ID using the questions index, turning it into a 5 digit number
 	digits = len(str(len(cqlist)))
@@ -32,6 +32,11 @@ def createid(n): # Creates an ID using the questions index, turning it into a 5 
 		return ('00000'+str(n))[-5:]
 	else:
 		return ('0'*digits+str(n))[-5:]
+
+def filewrite(dir,text):
+	f = open(dir,'w')
+	f.write(text)
+	f.close()
 
 
 customqs = open('customquestions.txt','r')
@@ -42,15 +47,25 @@ qqjet = '{"content":['
 n = 1
 
 for x in cqlist:
-	question = x.replace("'","\\u2019")
-	idnumber = '0'*2*len(str(len(cqlist)))+str(n)
-	qqitem = '{"x":false,"id":'+createid(n)+',"prompt":"'+question+'"},'
+	question = x.replace("'","\\u2019") # Replaces apostrophes in question to be readable by game code
+	idnumber = '0'*2*len(str(len(cqlist)))+str(n) # Question ID
+	audioid = idnumber+'_0' # ID for question audio file
+	questiondir = f'{newdir}/{idnumber}' # subdir for each question
+	os.mkdir(questiondir)
+	questiondata = '{"fields":[{"t":"B","v":"false","n":"HasJokeAudio"},{"t":"S","v":"","n":"Keywords"},{"t":"S","v":"","n":"Author"},{"t":"S","v":"","n":"KeywordResponseText"},{"t":"S","v":"'+question+'","n":"PromptText"},{"t":"S","v":"","n":"Location"},{"t":"A","n":"KeywordResponseAudio"},{"t":"A","v":"'+audioid+'","n":"PromptAudio"}]}'
+	filewrite(f'{questiondir}/data.jet',questiondata) # Writes questiondata to data.jet in question subdir
+	qqitem = '{"x":false,"id":'+createid(n)+',"prompt":"'+question+'"},' # Each question item for QuiplashQuestion.jet
 	qqjet = qqjet + qqitem
 	n+=1
 
 qqjet = qqjet[:-1] + '],"episodeid":1251}' # removes last comma in string and completes text
 
-qqjetfile = open(f'./{newdir}/QuiplashQuestions.jet','w')
-qqjetfile.write(qqjet)
-qqjetfile.close()
+filewrite(f'./{newdir}/QuiplashQuestion.jet',qqjet) # Write qqjet to file
+
+
+
+
+# qqjetfile = open(f'./{newdir}/QuiplashQuestion.jet','w')
+# qqjetfile.write(qqjet)
+# qqjetfile.close()
 
